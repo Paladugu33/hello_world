7df4fd4e8fc80d0e4518a8bbb2da7a53288a6ab2
Paladugu33 <38241488+Paladugu33@users.noreply.github.com>
2018-09-01 05:16:06 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import wordgraph
+import bfs2
+#from wordgraph import inputgraph
+
+import unittest
+#import words
+
+'''def test_inputgraph(unittest.TestCase):
+	return wordgraph.inputGraph()
+
+a = test_inputgraph()
+print (a)
+'''
+
+import os
+
+a=os.system('python3 wordgraph.py')
+
+print (a)
